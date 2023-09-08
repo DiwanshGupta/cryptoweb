75cc6f564788dc1e7941ddefea3a8f1d4f569edd
DiwanshGupta <130465365+DiwanshGupta@users.noreply.github.com>
2023-09-08 21:27:14 +0530

    Update footer.js

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className="bg-gradient-to-b  from-gray-500 md:ml-64 mb-0 to-gray-500 items-center flex-col justify-center m-auto flex">
+      <div className="border-t border-white md:ml-64 mb-0  items-center flex-col justify-center m-auto flex">
         <div className="text-4xl font-semibold  bg-gradient-to-r from-purple-400 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient">
           Cryptoweb
         </div>
